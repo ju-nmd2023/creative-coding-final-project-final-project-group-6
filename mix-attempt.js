7340cf285092316b16da7dd9698e44866b007a61
vasilenagg <149382184+vasilenagg@users.noreply.github.com>
2025-10-01 14:39:04 +0200

    background

--- mix-attempt.js
+++ mix-attempt.js
@@ -16,6 +16,7 @@ let particles = [];
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
+
   objectDetector = ml5.objectDetector("cocossd", {}, modelLoaded);
   // objectDetector = ml5.objectDetector("yolo", {}, modelLoaded);
   video = createCapture(VIDEO);
@@ -185,22 +186,26 @@ function gotDetection(err, results) {
 }
 
 function draw() {
-  background(0, 0, 0);
+  if (millis() > 10000) {
+    background(68, 49, 55);
+  } else {
+    background(0);
+  }
 
   let personDetected = objects.some(
     (obj) => obj.label === "person" && obj.confidence > 0.6
   );
 
-  for (let obj of objects) {
-    if (obj.confidence > 0.6) {
-      push();
-      fill(250, 226, 42);
-      rect(obj.x, obj.y, obj.width, obj.height);
-      pop();
-      push();
-      pop();
-    }
-  }
+  //for (let obj of objects) {
+  // if (obj.confidence > 0.6) {
+  //   push();
+  //fill(250, 226, 42);
+  //rect(obj.x, obj.y, obj.width, obj.height);
+  //pop();
+  //push();
+  //pop();
+  // }
+  //}
 
   // / Code borrowed from a website - BEGIN
   //Source: ChatGPT (2025) https://chatgpt.com/share/68d3e3be-f730-800a-8a18-dfeaedc7692a
@@ -214,6 +219,7 @@ function draw() {
       agent.checkBorders();
       agent.draw();
     }
+
     for (let particle of particles) {
       particle.update();
       particle.draw();
